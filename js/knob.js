908ad49d74a018a29b191362aa2c16c9037ec03d
SerhiiChynin <sergey.chinin@gmail.com>
2023-02-04 18:26:35 +0200

    template

---
+++ js/knob.js
@@ -0,0 +1,27 @@
+
+let b_1 = document.querySelector('.b_1');
+let b_2 = document.querySelector('.b_2');
+let b_3 = document.querySelector('.b_3');
+let b_4 = document.querySelector('.b_4');
+let b_5 = document.querySelector('.b_5');
+let b_6 = document.querySelector('.b_6');
+let b_7 = document.querySelector('.b_7');
+let b_8 = document.querySelector('.b_8');
+
+let out_1 = document.querySelector('.out_1');
+let out_2 = document.querySelector('.out_2');
+let out_3 = document.querySelector('.out_3');
+let out_4 = document.querySelector('.out_4');
+let out_5 = document.querySelector('.out_5');
+let out_6 = document.querySelector('.out_6');
+let out_7 = document.querySelector('.out_7');
+let out_8 = document.querySelector('.out_8');
+
+let i_1 = document.querySelector('.i_1');
+let i_2 = document.querySelector('.i_2');
+let i_3 = document.querySelector('.i_3');
+let i_4 = document.querySelector('.i_4');
+let i_5 = document.querySelector('.i_5');
+let i_6 = document.querySelector('.i_6');
+let i_7 = document.querySelector('.i_7');
+let i_8 = document.querySelector('.i_8');
---
+++ js/tips.js
@@ -0,0 +1,10 @@
+b_1.onclick = () => {
+    out_1.innerHTML = `Hello!`;
+    out_8.innerHTML = `Baby!`;
+    out_4.style.background = 'yellowgreen';
+    out_4.innerHTML = 'I LOVE';
+    out_5.style.background = 'yellowgreen';
+    out_5.innerHTML = 'YOU !';
+    out_1.style.background = 'yellowgreen';
+    out_8.style.background = 'yellowgreen';
+}
